78665cf1b8ee42b9f70d594a28e18d14a87480e7
Janelle Akinyi <janellekylieakinyi@gmail.com>
2026-04-15 09:30:58 +0300

    Add new assignment

---
+++ assignment1.py
@@ -0,0 +1,11 @@
+
+#Simple Calculator
+#This explains how the calculator works:
+#It divides two numbers
+#Asks user for input
+
+first_number = input ("first_number:" ) 
+second_number = input ("second_number:" )
+divide = first_number / second_number
+print ("result: divide")
+
