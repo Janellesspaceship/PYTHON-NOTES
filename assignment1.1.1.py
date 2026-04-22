35567d6ddce68b3510c5f897df59d1caa1290f65
Janelle Akinyi <janellekylieakinyi@gmail.com>
2026-04-22 10:57:40 +0300

    Add Question 1 of Assignmnet 1.1

---
+++ assignment1.1.1.py
@@ -0,0 +1,20 @@
+
+#Simple Calculator
+#This explains how the calculator works:
+#It divides two numbers
+#Asks user for input
+
+first_number = input ("first_number:" ) 
+second_number = input ("second_number:" )
+division = float("first_number" / "second_number)")
+print ("result: division")
+
+def add_numbers_inputed_by_user():
+
+    num1 = float(input("enter the first number:"))
+    num2 = float(input("enter the second number:"))
+    result = num1 + num2
+    print(f"The sum of {num1} and {num2} is {result}")
+
+add_numbers_inputed_by_user()
+
